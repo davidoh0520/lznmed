de6a6f2535e3bed1c840fcfaf1ea34bc92dc41c8
davidoh0520 <sales@lznmed.com>
2026-07-29 06:19:15 +0800

    Remove anti-fog liquid products (#89)
    Remove restricted anti-fog liquid, treatment, and applicator products from storefront and admin purchase sources while retaining solid anti-fog safety eyewear.

--- tools/catalog-product-exclusions.js
+++ tools/catalog-product-exclusions.js
@@ -1,10 +1,16 @@
 (function () {
+  var excludedPublicModels = new Set([
+    "LZN-TL-0018",
+    "LZN-TL-0019"
+  ]);
+
   var excludedSourceIds = new Set([
     "626769523006",
     "737807180518",
     "684620908475",
     "685253897109",
     "634149170915",
+    "670127834258",
     "595399753758",
     "674373644031",
     "607384329144"
@@ -12,8 +18,9 @@
 
   function isExcluded(item) {
     var model = String(item && (item.legacyModel || item.model) || "");
+    var publicModel = String(item && item.model || "");
     var sourceId = model.replace(/^LZN-/, "").split("-")[0];
-    return excludedSourceIds.has(sourceId);
+    return excludedPublicModels.has(publicModel) || excludedSourceIds.has(sourceId);
   }
 
   (window.CATALOG_DATA || []).forEach(function (category) {
@@ -34,7 +41,10 @@
   var purchaseSources = window.LZN_ADMIN_PURCHASE_SOURCES || {};
   Object.keys(purchaseSources).forEach(function (publicModel) {
     var source = purchaseSources[publicModel] || {};
-    if (excludedSourceIds.has(String(source.sourceProductId || ""))) {
+    if (
+      excludedPublicModels.has(publicModel) ||
+      excludedSourceIds.has(String(source.sourceProductId || ""))
+    ) {
       delete purchaseSources[publicModel];
     }
   });
