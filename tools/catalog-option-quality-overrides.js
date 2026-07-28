29cf8f8c810e1f76cbc18dd19525de443bf299f9
davidoh0520 <sales@lznmed.com>
2026-07-28 17:25:44 +0800

    Localize latest catalog images and remove FR-1009

--- tools/catalog-option-quality-overrides.js
+++ tools/catalog-option-quality-overrides.js
@@ -1,4 +1,18 @@
 (function () {
+  var cleanMainModels = [
+    "1001", "1002", "1003", "1004", "1005", "1006", "1008", "1010",
+    "1011", "1012", "1013", "1014", "1016", "1017", "1019", "1020",
+    "1021", "1022", "1023", "1024", "1025", "1026", "1027", "1028",
+    "1031", "1032", "1034", "1035", "1037", "1038", "1039", "1041",
+    "1042", "1043", "1044", "1045", "1046"
+  ];
+
+  var mainOverrides = cleanMainModels.reduce(function (result, model) {
+    result["LZN-TL-" + model] =
+      "/tools/assets/catalog/main-clean-english-20260728/tl" + model + ".webp";
+    return result;
+  }, {});
+
   var overrides = {
     "LZN-TL-1007": Array.from({ length: 27 }, function (_, index) {
       return (
@@ -23,6 +37,15 @@
     })
   };
 
+  function applyMain(item) {
+    var image = mainOverrides[item.model];
+    if (!image) return;
+
+    item.image = image;
+    item.title = image;
+    item.images = [image];
+  }
+
   function apply(item, options) {
     var images = overrides[item.model];
     if (!images) return;
@@ -40,6 +63,7 @@
 
   (window.CATALOG_DATA || []).forEach(function (category) {
     (category.items || []).forEach(function (item) {
+      applyMain(item);
       apply(item, item.options);
     });
   });
@@ -51,6 +75,7 @@
 
   series.forEach(function (group) {
     (group.items || []).forEach(function (item) {
+      applyMain(item);
       apply(item, item.colors);
     });
   });
